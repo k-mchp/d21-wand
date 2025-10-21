444ce08d1ae916d72fa5146152bf6df9dc5c0db2
Kelvin LeBeaux - C51209 <Kelvin.LeBeaux@microchip.com>
2025-10-21 09:19:41 -0400

    Update Documentation

--- wand_firmware/src/app_config.h
+++ wand_firmware/src/app_config.h
@@ -64,7 +64,7 @@ static bool wifi_mode;
 
 // Data streaming formatting selection
 #ifndef DATA_STREAMER_FORMAT
-#define DATA_STREAMER_FORMAT    DATA_STREAMER_FORMAT_NONE
+#define DATA_STREAMER_FORMAT    DATA_STREAMER_FORMAT_MDV
 #endif
 
 // IMU sampling rate in units of SNSR_SAMPLE_RATE_UNIT
